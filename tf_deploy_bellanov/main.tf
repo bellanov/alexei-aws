da0dfc8487f676ea5daca8ac08773f4216970edf
Bellanov Apilli <BellanovApilli@gmail.com>
2023-02-09 21:11:25 -0500

    AWS-1: Hello Bellanov

---
+++ tf_deploy_bellanov/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.50.0"
+    }
+  }
+}
+
+provider "aws" {
+  region      = local.region
+}
+
+# module "storage" {
+#   source   = "../modules/storage"
+#   for_each = local.manifest
+#   project  = local.project
+#   location = local.location
+# }
+
+locals {
+  region   = "us-east1"
+
+  manifest = {
+    "dev" : {},
+    "staging" : {},
+    "prod" : {}
+  }
+}
