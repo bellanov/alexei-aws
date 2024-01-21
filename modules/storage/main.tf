25dfa37a87666feef767c4760365832e64a35eb0
Bellanov Apilli <bellanovapilli@gmail.com>
2024-01-20 20:01:46 -0500

    Restricted public access to S3 buckets

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -14,11 +14,12 @@ resource "aws_s3_bucket" "s3" {
   }
 }
 
-# resource "aws_s3_bucket_public_access_block" "example" {
-#   bucket = aws_s3_bucket.example.id
+resource "aws_s3_bucket_public_access_block" "s3" {
+  for_each = aws_s3_bucket.s3
+  bucket = each.value.bucket
 
-#   block_public_acls       = true
-#   block_public_policy     = true
-#   ignore_public_acls      = true
-#   restrict_public_buckets = true
-# }
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
