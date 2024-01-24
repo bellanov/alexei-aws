101f4a361ec13dfd131aa54cef135123931fe383
Bellanov Apilli <bellanovapilli@gmail.com>
2024-01-23 22:03:25 -0500

    Added cidr_block to VPC outputs

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,7 +1,7 @@
 
 output "vpcs" {
   description = "VPCs."
-  value       = { for vpc in aws_vpc.vpc : vpc.tags.Name => tomap({ "id" = vpc.id, "route_table_id" = vpc.main_route_table_id, "security_group_id" = vpc.default_security_group_id }) }
+  value       = { for vpc in aws_vpc.vpc : vpc.tags.Name => tomap({ "id" = vpc.id, "cidr_block" = vpc.cidr_block, "route_table_id" = vpc.main_route_table_id, "security_group_id" = vpc.default_security_group_id }) }
 }
 
 output "subnets" {
