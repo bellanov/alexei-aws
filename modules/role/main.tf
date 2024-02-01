b1fadaf7e0e2e97b7a2367f33e7fc5f63ec81efb
Bellanov Apilli <bellanovapilli@gmail.com>
2024-02-01 08:36:04 -0500

    Added TODOs for role / policy module

--- modules/role/main.tf
+++ modules/role/main.tf
@@ -5,7 +5,34 @@
 #   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 # }
 
+# assume_role_policy is required so consider parmeterizing here
+# rather than polluting environments with data {} blocks
+# resource "aws_iam_role" "test_role" {
+#   name = "test_role"
+
+#   # Terraform's "jsonencode" function converts a
+#   # Terraform expression result to valid JSON syntax.
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Action = "sts:AssumeRole"
+#         Effect = "Allow"
+#         Sid    = ""
+#         Principal = {
+#           Service = "ec2.amazonaws.com"
+#         }
+#       },
+#     ]
+#   })
+
+#   tags = {
+#     tag-key = "tag-value"
+#   }
+# }
+
 # resource "aws_iam_role_policy_attachment" "test-attach" {
 #   role       = aws_iam_role.role.name
 #   policy_arn = aws_iam_policy.policy.arn
 # }
+
